177dce7578917b4ff06b266ed5e58cd9a8b6db56
Tasya Harum <sctasyaharum@gmail.com>
2025-08-25 12:57:37 +0900

    Upload Pertama Project

---
+++ tasyamobile/bin/tasyamobile.dart
@@ -0,0 +1,5 @@
+import 'package:tasyamobile/tasyamobile.dart' as tasyamobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tasyamobile.calculate()}!');
+}
---
+++ tasyamobile/test/tasyamobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:tasyamobile/tasyamobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
